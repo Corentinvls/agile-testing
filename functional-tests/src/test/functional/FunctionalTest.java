6d7ffbba1c0fe1454b79bd82f17da4048a9a29ed
RomanCLV <roman.clavier.2001@gmail.com>
2021-01-11 19:40:44 +0100

    fuck les contains

--- functional-tests/src/test/functional/FunctionalTest.java
+++ functional-tests/src/test/functional/FunctionalTest.java
@@ -1,15 +1,24 @@
 package test.functional;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -19,20 +28,18 @@ public class FunctionalTest {
 
     @Before
     public void setUp() throws Exception {
-
         String OS = System.getProperty("os.name").toLowerCase();
-        System.out.println(OS);
         System.setProperty("webdriver.chrome.driver", OS.contains("win") ? "chromedriver.exe" : "/Library/Java/JUNIT/chromedriver");
 		driver = new ChromeDriver();
-	    	// Seems no more working in last Chrome versions
+		// Seems no more working in last Chrome versions
 		// driver.manage().window().maximize();
   		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
      }
 
     // Test de la Story #1-homepage (https://trello.com/c/WKTneu9o/1-homepage)
+
 	@Test
     public void testHomepage() throws Exception {
-
         String h1 = "/html/body/div[1]/div/div[5]/div[3]/main/div[1]/div/section/div/div[1]/h1/span";
         String subH1 = "/html/body/div[1]/div/div[5]/div[3]/main/div[1]/div/section/div/div[2]/p/span";
         String joinButton = "/html/body/div[1]/div/div[5]/div[3]/main/div[1]/div/section/div/div[3]/a";
@@ -49,18 +56,60 @@ public class FunctionalTest {
     @Test
     public void testResearch() throws Exception {
 
-        String h1 = "/html/body/div[3]/div[1]/div[1]/h1";
-        String navBar = "/html/body/div[3]/div[1]/div[4]/div[1]";
+        String h1XPath = "/html/body/div[3]/div[1]/div[1]/h1";
 
         driver.get("https://www.meetup.com/fr-FR/find/outdoors-adventure/");
 
-        WebElement navbarComponent = driver.findElement(By.xpath(navBar));
+        WebElement navBar = driver.findElement(By.id("findNavBar"));
 
         assertTrue(driver.getTitle().contains("Nature et aventure"));
-        assertTrue(driver.findElement(By.xpath(h1)).getText().contains("Nature et aventure"));
+        assertTrue(driver.findElement(By.xpath(h1XPath)).getText().contains("Nature et aventure"));
 
-        assertTrue(navbarComponent.isEnabled());
-        assertTrue(navbarComponent.findElement(By.id("mainKeywords")).isEnabled());
+        assertTrue(navBar.isEnabled());
+        assertTrue(navBar.findElement(By.id("mainKeywords")).isEnabled());
+        assertTrue(navBar.findElement(By.id("simple-radius")).isEnabled());
+        assertTrue(navBar.findElement(By.id("simple-location")).isEnabled());
+        assertTrue(navBar.findElement(By.id("simple-view-selector")).isEnabled());
+    }
+
+    @Test
+    public void testSort() throws Exception {
+        driver.get("https://www.meetup.com/fr-FR/find/outdoors-adventure/");
+
+        WebElement findOrder = driver.findElement(By.id("simple-find-order"));
+        WebElement anchor = findOrder.findElement(By.tagName("a"));
+        WebElement span = anchor.findElement(By.tagName("span"));
+        assertEquals(span.getText(), "pertinence");
+    }
+
+    @Test
+    public void testSortChoices() throws Exception {
+        driver.get("https://www.meetup.com/fr-FR/find/outdoors-adventure/");
+
+        WebElement findOrder = driver.findElement(By.id("simple-find-order"));
+        WebElement ul = findOrder.findElement(By.tagName("ul"));
+        List<WebElement> items = ul.findElements(By.tagName("li"))
+                .stream()
+                .filter(item -> !item.getAttribute("class").equals("display-none"))
+                .collect(Collectors.toList());
+
+        assertEquals(items.size(), 4);
+
+        List<String> choices = Arrays.asList("pertinence", "plus récents", "nombre de membres", "proximité");
+
+        for (WebElement item : items) {
+            System.out.println("Start\n");
+            for (String s: choices) {
+                System.out.println("t:" + s);
+            }
+            WebElement anchor = item.findElement(By.tagName("a"));
+            String txt = anchor.getAttribute("data-copy");
+            System.out.println("text: '" + txt + "'");
+            assertThat(choices, hasItem(txt));
+            choices.remove(txt);
+            System.out.println("\nEnd\n");
+
+        }
     }
 
     @After
