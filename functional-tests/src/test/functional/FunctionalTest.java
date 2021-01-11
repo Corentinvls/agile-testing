eb0e294de65216e6a5ba6815b645d4d817007b7c
Dreaxxx <ldqtheone@gmail.com>
2021-01-11 16:40:53 +0100

    functionnal testing

--- functional-tests/src/test/functional/FunctionalTest.java
+++ functional-tests/src/test/functional/FunctionalTest.java
@@ -1,18 +1,17 @@
 package test.functional;
 
-import java.util.concurrent.TimeUnit;
-
-import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
-
-import static org.junit.Assert.*;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class FunctionalTest {
 
@@ -20,7 +19,10 @@ public class FunctionalTest {
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver","/Library/Java/JUNIT/chromedriver");
+
+        String OS = System.getProperty("os.name").toLowerCase();
+        System.out.println(OS);
+        System.setProperty("webdriver.chrome.driver", OS.contains("win") ? "chromedriver.exe" : "/Library/Java/JUNIT/chromedriver");
 		driver = new ChromeDriver();
 	    	// Seems no more working in last Chrome versions
 		// driver.manage().window().maximize();
@@ -30,15 +32,36 @@ public class FunctionalTest {
     // Test de la Story #1-homepage (https://trello.com/c/WKTneu9o/1-homepage)
 	@Test
     public void testHomepage() throws Exception {
+
+        String h1 = "/html/body/div[1]/div/div[5]/div[3]/main/div[1]/div/section/div/div[1]/h1/span";
+        String subH1 = "/html/body/div[1]/div/div[5]/div[3]/main/div[1]/div/section/div/div[2]/p/span";
+        String joinButton = "/html/body/div[1]/div/div[5]/div[3]/main/div[1]/div/section/div/div[3]/a";
+
         driver.get("https://www.meetup.com/fr-FR/");
 		assertEquals(driver.getTitle(), "Partagez vos passions | Meetup");
-		// TODO
-		// To Be Completed By Coders From Coding Factory
+		assertEquals(driver.findElement(By.xpath(h1)).getText(), "Le monde vous tend les bras");
+		assertEquals(driver.findElement(By.xpath(subH1)).getText(), "Rejoignez un groupe local pour rencontrer du monde, tester une nouvelle activité ou partager vos passions.");
+		assertEquals(driver.findElement(By.xpath(joinButton)).getCssValue("background"), "rgb(241, 58, 89) none repeat scroll 0% 0% / auto padding-box border-box");
+		assertEquals(driver.findElement(By.xpath(joinButton)).getText(), "Rejoindre Meetup");
+		assertEquals(driver.findElement(By.xpath(joinButton)).getAttribute("href"), "https://www.meetup.com/fr-FR/register/");
     }
 
-    // Test de la Story n ...
-    // TODO
-    // To Be Completed By Coders From Coding Factory
+    @Test
+    public void testResearch() throws Exception {
+
+        String h1 = "/html/body/div[3]/div[1]/div[1]/h1";
+        String navBar = "/html/body/div[3]/div[1]/div[4]/div[1]";
+
+        driver.get("https://www.meetup.com/fr-FR/find/outdoors-adventure/");
+
+        WebElement navbarComponent = driver.findElement(By.xpath(navBar));
+
+        assertTrue(driver.getTitle().contains("Nature et aventure"));
+        assertTrue(driver.findElement(By.xpath(h1)).getText().contains("Nature et aventure"));
+
+        assertTrue(navbarComponent.isEnabled());
+        assertTrue(navbarComponent.findElement(By.id("mainKeywords")).isEnabled());
+    }
 
     @After
     public void tearDown() throws Exception {
