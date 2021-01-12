9f1c37898ca05ffeed66f522af0062196ac8c8c8
Corentinvls <corentin.vallois@gmail.com>
2021-01-12 16:39:03 +0100

    done

--- functional-tests/src/test/functional/MeetUpDetailedView.java
+++ functional-tests/src/test/functional/MeetUpDetailedView.java
@@ -8,9 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.time.Duration;
 import java.util.Arrays;
