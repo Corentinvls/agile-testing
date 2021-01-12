3e0626aff688e4d846c4c6078a0f9b416bf2e04d
Corentinvls <corentin.vallois@gmail.com>
2021-01-12 16:40:40 +0100

    done

--- functional-tests/src/test/functional/MeetUpDetailedView.java
+++ functional-tests/src/test/functional/MeetUpDetailedView.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 
-import java.time.Duration;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
