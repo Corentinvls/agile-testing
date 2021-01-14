06edc6f68407165a95669be720743273707307a5
Corentinvls <corentin.vallois@gmail.com>
2021-01-14 16:06:47 +0100

    report in prog

--- tesla-test/src/test/acceptance/config/ConfigpageTest.java
+++ tesla-test/src/test/acceptance/config/ConfigpageTest.java
@@ -6,9 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-	features = {"../src/test/acceptance/config"}, // ou se situe votre fichier .feature
-	plugin = {"pretty"}
-	)
+		features = {"src/test/acceptance/config"}, // ou se situe votre fichier .feature
+		plugin = { "pretty", "json:cucumber-reports/config.json" }
+		)
+
 public class ConfigpageTest {
 
 }
--- tesla-test/src/test/acceptance/home/HomepageTest.java
+++ tesla-test/src/test/acceptance/home/HomepageTest.java
@@ -7,9 +7,9 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-	features = {"../src/test/acceptance"}, // ou se situe votre fichier .feature
-	plugin = {"pretty"}
-	)
+		features = {"src/test/acceptance/home"}, // ou se situe votre fichier .feature
+		plugin = { "pretty", "json:cucumber-reports/home.json" }
+)
 public class HomepageTest {
 
 }
